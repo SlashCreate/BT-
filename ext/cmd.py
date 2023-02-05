f731714295da0edd3ac84b42e4953e166c919806
SlashCreate <112573796+SlashCreate@users.noreply.github.com>
2023-02-05 16:23:47 -0500

    custom commands

--- ext/cmd.py
+++ ext/cmd.py
@@ -1,1 +1,11 @@
+# librarys your command(s) use:
+import os
 
+def command(cmd, args):
+  # Use for custom commands
+  # example:
+  # custom #hello
+  
+  if cmd == 'custom':
+    # args[0] is the same as command name
+    print(args[1])
