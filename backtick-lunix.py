37d0fca3505cc101834cd3e3350af584060fc330
SlashCreate <112573796+SlashCreate@users.noreply.github.com>
2023-02-05 16:20:09 -0500

    backtick for Lunix

---
+++ backtick-lunix.py
@@ -0,0 +1,36 @@
+# BackTick v1.0.0
+# https://github.com/SlashCreate/BackTick-for-python
+
+import os
+import time
+
+def clr():
+  os.system('clear')
+  
+def command(cmd, args):
+  # Use for custom commands
+  # example:
+  # custom #hello
+  
+  if cmd == 'custom':
+    # args[0] is the same as command name
+    print(args[1])
+    
+while True:
+  a = input('>> ').split(' #')
+  if a[0] == 'help':
+    # help screen
+    print('''
+    help              help screen
+    print #<text>     print <text> onto screen
+    clr               clear screen
+    exit              exit BackTick command line
+    ''')
+  elif a[0] == 'print':
+    # print <text>
+    print(a[1])
+  elif a[0] == 'clr':
+    clr()
+  elif a[0] == 'exit':
+    clr()
+    break
